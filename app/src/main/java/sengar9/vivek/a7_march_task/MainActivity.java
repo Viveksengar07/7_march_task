7bd89f64f9fb00db88a4f54f053600eee5b2e4e5
Viveksengar07 <vivek.sengar_mca18@gla.ac.in>
2019-03-07 10:45:44 +0530

    init

---
+++ app/src/main/java/sengar9/vivek/a7_march_task/MainActivity.java
@@ -0,0 +1,13 @@
+package sengar9.vivek.a7_march_task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
